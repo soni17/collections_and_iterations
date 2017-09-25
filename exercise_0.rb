31e1a0d5157b9cce2d346fd842acfc716dbd8072
soni17 <elb004@gmail.com>
2017-09-25 16:13:14 -0400

    added hashes

--- exercise_0.rb
+++ exercise_0.rb
@@ -1,19 +1,53 @@
 
+#arrays
 fav_colours = ["blue","green","black","navy"]
-
 ages = [31,36,40,55]
-
 coin_flips = ["heads", "not heads", "not heads", "heads", "heads"]
-
 artists = ["band1","band2","band3"]
-
 colors = [:blue, :green , :black, :navy ]
 
+#print arrays
 print "#{fav_colours} \n"
 print "#{ages} \n"
 print "#{coin_flips} \n"
 print "#{artists} \n"
 print "#{colors} \n"
+
+#hashes
+definitions = {
+  :obviously => "in a way that is easily perceived or understood; clearly.",
+  :dejavu => "the illusion of remembering scenes and events when experienced for the first time. 
+              A feeling that one has seen or heard something before.",
+  :interpolate => "To alter or corrupt (something, such as a text) by inserting new or foreign matter.
+	           To insert (words) into a text or into a conversation."
+}
+
+movies = {
+  :They_Live => "1988"
+  :The_Matrix => "1999"
+  :The_Usual_Suspects => "1995"
+}
+
+cities = {
+  :London => "8.7 Million"
+  :Paris => "10 Million"
+  :Rome = > "2.2 Million"
+}
+
+names{
+  :Tom => 25
+  :Timmy => 27
+  :John => 41
+  :Steven => 45
+}
+
+
+
+
+
+
+
+
 
 
 
