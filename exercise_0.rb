6f98f9433f18446a0460fbf1e7fb3d44a9f355f9
soni17 <elb004@gmail.com>
2017-09-25 15:19:04 -0400

    completed exercise 0

---
+++ exercise_0.rb
@@ -0,0 +1,20 @@
+
+fav_colours = ["blue","green","black","navy"]
+
+ages = [31,36,40,55]
+
+coin_flips = ["heads", "not heads", "not heads", "heads", "heads"]
+
+artists = ["band1","band2","band3"]
+
+colors = [:blue, :green , :black, :navy ]
+
+print "#{fav_colours} \n"
+print "#{ages} \n"
+print "#{coin_flips} \n"
+print "#{artists} \n"
+print "#{colors} \n"
+
+
+
+
