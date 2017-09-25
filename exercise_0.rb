9ad6073a8615591bf0fe79e9f374b0b08f23afbf
soni17 <elb004@gmail.com>
2017-09-25 16:20:09 -0400

    removed print commands for the arrays

--- exercise_0.rb
+++ exercise_0.rb
@@ -6,13 +6,6 @@ coin_flips = ["heads", "not heads", "not heads", "heads", "heads"]
 artists = ["band1","band2","band3"]
 colors = [:blue, :green , :black, :navy ]
 
-#print arrays
-print "#{fav_colours} \n"
-print "#{ages} \n"
-print "#{coin_flips} \n"
-print "#{artists} \n"
-print "#{colors} \n"
-
 #hashes
 definitions = {
   :obviously => "in a way that is easily perceived or understood; clearly.",
